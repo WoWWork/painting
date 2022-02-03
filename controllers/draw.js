44bf847046e214b224074018a7dab4532092046d
Joey <43963327+WoWWork@users.noreply.github.com>
2022-02-04 03:00:39 +0800

    Add files via upload

---
+++ controllers/draw.js
@@ -0,0 +1,340 @@
+const canvas = document.getElementById('canvas')
+const b_save = document.getElementById('save')
+const r_width = document.getElementById('width')
+const r_red = document.getElementById('red')
+const r_green = document.getElementById('green')
+const r_blue = document.getElementById('blue')
+const t_btn = document.getElementById('btn_text')
+
+const ctx = canvas.getContext('2d')
+
+let data_x = new Array();
+let data_y = new Array();
+let data_w = 1;
+let data_c = new Array([0, 0, 0]);
+
+  /* document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0'  ',minimum-scale=0.5'  ',maximum-scale=2'  ',user-scalable=yes">'); */
+
+const init = () => {
+  let gap_y = document.getElementById('icon-bar').offsetHeight;
+  let view_w = document.body.clientWidth
+  let view_h = document.body.clientHeight
+  if(document.body.clientWidth > 750)
+  {
+    canvas.setAttribute("width", view_w * 0.75);
+    canvas.setAttribute("height", view_h - gap_y);
+  }
+  else
+  {
+    canvas.setAttribute("width", view_w);
+    canvas.setAttribute("height", view_h * 0.7 - gap_y);
+  }
+  let url = '/frame'
+  let xhr = new XMLHttpRequest()
+  xhr.open("POST", url, true)
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status == 200) {
+      console.log('done')
+    }
+  }
+  xhr.send('view_w=' + canvas.clientWidth + '&view_h=' + canvas.clientHeight)
+}
+
+  /* start coordination */
+  
+let x1= 0
+let y1= 0
+
+  /* end coordination */
+  
+let x2= 0
+let y2= 0
+  
+  /* declare a hasTouchEvent variable，to check for touch event existing */
+
+const hasTouchEvent = 'ontouchstart' in window ? true : false
+
+  /* depend on hasTouchEvent decisive to monitor mouse or touch event */
+  
+const downEvent = hasTouchEvent ? 'ontouchstart' : 'mousedown'
+const moveEvent = hasTouchEvent ? 'ontouchmove' : 'mousemove'
+const upEvent = hasTouchEvent ? 'touchend' : 'mouseup'
+
+  /* declare isMouseActive for mouse status，for check mouse in mousedown status and trigger mousemove status. */
+
+let isMouseActive = false
+  
+window.addEventListener('resize', function(e){
+  init();
+}, true)  
+  
+  /* trigger mouse down event */
+
+canvas.addEventListener(downEvent, function(e){
+  isMouseActive = true  
+  
+  let gap_y = document.getElementById('icon-bar').offsetHeight;
+  
+  x1 = e.clientX + document.body.scrollLeft
+  y1 = e.clientY - gap_y + document.body.scrollTop
+  
+  data_x.push(x1);
+  data_y.push(y1);
+  
+  ctx.lineWidth = data_w
+  ctx.lineCap = 'round'
+  ctx.lineJoin = 'round'
+})
+  
+  /* trigger mouse move event */
+  
+canvas.addEventListener(moveEvent, function(e){
+  if(!isMouseActive) return;
+  
+  let gap_y = document.getElementById('icon-bar').offsetHeight;
+  
+  /* get end coordinate */
+  
+  x2 = e.clientX + document.body.scrollLeft
+  y2 = e.clientY - gap_y + document.body.scrollTop
+    
+  data_x.push(x2);
+  data_y.push(y2);
+    
+  /* start drawing */
+  
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.stroke()
+
+  /* update start coordinate */
+  
+  x1 = x2
+  y1 = y2
+    
+})
+  
+  /* trigger mouse up event */
+  
+canvas.addEventListener(upEvent, function(e){  
+  isMouseActive = false;
+  
+  //send_data();
+  send_data_db();
+})
+
+  /* trigger text button event */
+
+t_btn.addEventListener('click', function(e){
+  
+  //send_text();
+  send_text_db();
+})
+
+  /* adjust pen width */
+  
+const width_change = () => {
+  data_w = r_width.value
+}
+  
+  /* adjust pen color */
+  
+const color_change = () => {
+  let r = r_red.value
+  let g = r_green.value
+  let b = r_blue.value
+  data_c = [r, g, b]
+  ctx.strokeStyle = "rgb("+ r +"," + g + "," + b + ")";
+}
+
+  /* get text data through LAN */
+
+const get_text = async (data) => {
+  let url = "http://" + data + ':3000/g_text';
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      console.log(xhr.responseText);
+      let data_list = JSON.parse(xhr.responseText);
+      if(data_list.message.length != 0){
+        display_text(data_list)
+      }
+    }
+  }
+}
+
+  /* read text from MongoDB */
+
+const read_text_db = async (data) => {
+  let url = "http://" + data + ':3000/db_read_t';
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      console.log(xhr.responseText);
+      let data_list = JSON.parse(xhr.responseText);
+      if(data_list.length != 0){
+        display_text(data_list);
+      }
+    }
+  };
+  xhr.send();
+};
+
+  /* send text through LAN */
+
+const send_text = () => {
+  let message = document.getElementById('text_input').value;
+  let url = '/s_text';
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status == 200) {
+      console.log('done');
+    }
+  }
+  xhr.send('message=' + message);
+};
+
+  /* send text to DB */
+
+const send_text_db = () => {
+  let message = document.getElementById('text_input').value;
+  let url = '/db_write_t';
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status == 200) {
+      console.log('done');
+    }
+  }
+  xhr.send('message=' + message);
+};
+
+  /* sending data through LAN */
+
+const send_data = () => {
+  let url = '/s_data';
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status == 200) {
+      console.log('done!!');
+      data_x=[];
+      data_y=[];
+      data_w=[];
+      data_c=[];
+    }
+  }
+  xhr.send('data_x=' + data_x + '&data_y=' + data_y + '&data_w=' + data_w + '&data_c=' + data_c);
+};
+  
+  /* get corrdinate data through LAN */
+
+const get_data = async (data) => {
+  let url = "http://" + data + ':3000/g_data';
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      console.log(xhr.responseText);
+      let data_list = JSON.parse(xhr.responseText);
+      if(data_list.data_x.length != 0 && data_list.data_y.length != 0){
+        console.log(data_list);
+        drawing(data_list);
+      }
+    }
+  }
+  xhr.send();
+};
+
+  /* read corrdinate data from MongoDB */
+
+const read_data_db = async (data) => {
+  let url = "http://" + data + ':3000/db_read_c';
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      console.log(xhr.responseText);
+      let data_list = JSON.parse(xhr.responseText);
+      if(data_list[0].data_x.length != 0 && data_list[0].data_y.length != 0){
+        console.log(data_list)
+        drawing(data_list);
+      }
+    }
+  };
+  xhr.send();
+};
+  
+  /* sending coordinate data to MongoDB */
+  
+const send_data_db = () => {
+  let url = '/db_write_c';
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status == 200) {
+      console.log('send_data_db : done!!');
+      data_x=[];
+      data_y=[];
+      data_w=[];
+      data_c=[];
+    }
+  }
+  xhr.send('data_x=' + data_x + '&data_y=' + data_y + '&data_w=' + data_w + '&data_c=' + data_c);
+}
+
+  /* draw on canvas */
+
+const drawing = (data) => {
+  let px = data[0].data_x.split(',');
+  let py = data[0].data_y.split(',');
+  ctx.beginPath();
+  ctx.lineWidth = data[0].data_w;
+  ctx.lineCap = 'round';
+  ctx.lineJoin = 'round';
+  ctx.strokeStyle = "rgb("+ data[0].data_c[0] + "," + data[0].data_c[1] + "," + data[0].data_c[2] + ")";
+  for(let i = 0; i < px.length - 1; i++)
+  {
+    ctx.moveTo(px[i] * canvas.clientWidth, py[i] * canvas.clientHeight);
+    ctx.lineTo(px[i + 1] * canvas.clientWidth, py[i + 1] * canvas.clientHeight);
+    
+  }
+  ctx.stroke();
+  ctx.closePath();
+  width_change();
+  color_change();
+};
+
+const display_text = (data) => {
+  //console.log('display : ' + data[0].message);
+  document.getElementById("chat_content").value += "\r\n" + data[0].message;
+  document.getElementById("text_input").value = "";
+};
+
+  /* get current time */
+
+const time_refresh = async () => {
+  let date_time = new Date();
+};
+
+  /* update event collection */
+
+const update_set = async () => {
+  //await setInterval("time_refresh()", 1000);
+  //await setInterval("get_text(document.title)", 1000);
+  await setInterval("read_text_db(document.title)", 1000);
+  //await setInterval("get_data(document.title)", 1000);
+  await setInterval("read_data_db(document.title)", 1000);
+};
+
+init();
+update_set();
---
+++ controllers/setting.js
@@ -0,0 +1,140 @@
+document.getElementById('server').addEventListener('click', serverSet);
+document.getElementById('client').addEventListener('click', clientSet);
+
+/* Be Server Setting */
+  
+function serverSet()
+{
+  document.getElementById('config').innerHTML = "";
+  let game = document.createElement('select');
+  game.id = 'game';
+  let op = document.createElement('option');
+  op.value = 'draw';
+  op.text = 'draw';
+  game.appendChild(op);
+  let text_game = document.createTextNode('type : ');
+  let t_tag = document.createElement("h5");
+  t_tag.id = 'h_tag';
+  t_tag.appendChild(text_game);
+  t_tag.appendChild(game);
+  let name_t = document.createElement('input');
+  name_t.id = 'name_t';
+  let text_name = document.createTextNode('name : ');
+  let n_tag = document.createElement('h5');
+  n_tag.id = 'n_tag';
+  n_tag.appendChild(text_name);
+  n_tag.appendChild(name_t);
+  let btn = document.createElement('BUTTON');
+  btn.innerHTML = 'Confirm';
+  btn.addEventListener('click', function(){ checkSet(game.options[game.selectedIndex].text, name_t.value) });
+  document.getElementById('config').appendChild(t_tag);
+  document.getElementById('config').appendChild(n_tag);
+  document.getElementById('config').appendChild(btn);
+}
+
+/* Be Client Setting */
+
+function clientSet()
+{
+  document.getElementById('config').innerHTML = "";
+  let ip_t = document.createElement('input');
+  ip_t.id = 'ip_t';
+  let text_ip = document.createTextNode('connect : ');
+  let i_tag = document.createElement('h5');
+  i_tag.id = 'i_tag';
+  i_tag.appendChild(text_ip);
+  i_tag.appendChild(ip_t);
+  let name_t = document.createElement('input');
+  name_t.id = 'name_t';
+  let text_name = document.createTextNode('name : ');
+  let n_tag = document.createElement('h5');
+  n_tag.id = 'n_tag';
+  n_tag.appendChild(text_name);
+  n_tag.appendChild(name_t);
+  let btn = document.createElement('BUTTON');
+  btn.innerHTML = 'Confirm';
+  btn.addEventListener('click', function(){ connect(ip_t.value, name_t.value) });
+  document.getElementById('config').appendChild(i_tag);
+  document.getElementById('config').appendChild(n_tag);
+  document.getElementById('config').appendChild(btn);
+}
+  
+/* Page Option Check */  
+  
+function checkSet(type, name)
+{
+  if(!name || !type) return;
+  if(type === 'draw') {
+	  setCookie('game_type', type);
+	  setCookie('state', 'host');
+	  setCookie('user', name);
+	  local_info('host');
+  }
+  else window.location.href = '/404';
+}
+  
+function local_info(state)
+{
+  let url = 'https://www.cloudflare.com/cdn-cgi/trace';
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+  //xhr.setRequestHeader("Content-Type", "application/text");
+  xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    //console.log(xhr.status);
+    let ip_data = xhr.responseText.toString();
+    data = ip_data.split("\n");
+    data.forEach((element) => {
+	    let str = element.replace(/\s*;\s*/, ' ').replace(/\s*;\s*/, ' ').replace(/\s*;\s*/, ' ');
+	    let pair = str.split('=');
+	    if(pair[0] != 'h') setCookie(pair[0], pair[1]);
+    });
+    if(state == 'host') window.location.href = '/draw'
+    else if(state == 'client') window.location.href = '/join';
+	  else console.log(xhr.status);
+  }};
+  //var data = '{"login":"my_login","password":"my_password"}';
+  xhr.send(); 
+}
+
+/* connecting infomation */
+
+function connect(ip, name)
+{
+	if(!name || !ip) return;
+  
+  /* LAN Connection Method */
+	
+  let regx = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  if(regx.test(ip)) {
+	  setCookie('user', name);
+    setCookie('state', 'client');
+	  setCookie('connect', ip);
+	  local_info('client');
+	}
+ 
+  /* DB Connection Method */
+  /*
+  let regx = /^[A-Za-z0-9]{3,10}$/;
+  if(regx.test(ip)) {
+    setCookie('user', name);
+    setCookie('state', 'client');
+    setCookie('pwd', ip);
+    local_info('client');
+  }
+  */
+	else window.location.href = '/404';
+}
+
+/* cookie lifetime setting */
+
+function setCookie(name, value)
+{
+  if(!name || !value) return;
+  let d = new Date();
+  d.setTime(d.getTime() + (60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = `${name}=${value};${expires};path=/`;
+  let Enabled = document.cookie.indexOf(name+"=") != -1;
+  if(!Enabled) console.log('cookie not setting!');
+}
